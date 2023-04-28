6013b8023f4056d26f2f988b333e7da8b96de582
renancavalcantercb <renancavalcantercb@protonmail.com>
2023-04-27 21:54:41 -0300

    added toastify and new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Grid from './components/Grid';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        pauseOnHover={false}
+        closeOnClick
+        draggable
+      />
       <NavBar />
 
       <Routes>
         <Route path="/" element={<Grid />} />
-        <Route path="/produtos" element={<Grid />} />
+        <Route path="/sales" element={<Grid />} />
+        <Route path="/cart" />
       </Routes>
 
     </BrowserRouter>
