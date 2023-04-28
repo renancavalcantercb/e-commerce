1f985a5596bca7959044c9c7ac07ca73a13e876b
renancavalcantercb <renancavalcantercb@protonmail.com>
2023-04-27 21:56:31 -0300

    added sale, discount and hasDiscount price on generateItems

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -2,11 +2,15 @@ function generateItems(numItems) {
     const items = [];
 
     for (let i = 1; i <= numItems; i++) {
+        const hasDiscount = Math.random() > 0.5;
         const item = {
             id: i,
             title: `Item ${i}`,
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
             price: Math.floor(Math.random() * 100) + 1,
+            sale: hasDiscount,
+            discount: hasDiscount ? Math.floor(Math.random() * 50) + 1 : 0,
+            hasDiscount: hasDiscount,
             image: 'https://via.placeholder.com/150',
         };
 
@@ -15,5 +19,6 @@ function generateItems(numItems) {
 
     return items;
 }
+
 
 export { generateItems };
