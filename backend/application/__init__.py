91304ee19ae8d4fd3ad17a991af636dfb2fb1bc3
renancavalcantercb <renancavalcantercb@protonmail.com>
2023-05-22 15:42:16 -0300

    fix cors configuration

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -12,11 +12,15 @@ CORS(app)
 app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
 app.config['MONGO_URI'] = environ.get('MONGO_URI')
 
-
 client = MongoClient(app.config["MONGO_URI"], connect=False)
-
 db = client['e-commerce']
-
 products = db['products']
 
-from application import routes
+@app.after_request
+def add_cors_headers(response):
+    response.headers.add('Access-Control-Allow-Origin', request.headers.get('Origin', '*'))
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    return response
+
+from application.routes import *
