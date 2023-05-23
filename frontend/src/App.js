65c8e138e80b26504299388b27d7ea4938e8a694
renancavalcantercb <renancavalcantercb@protonmail.com>
2023-05-23 12:32:17 -0300

    added Provider for redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,33 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import NavBar from './components/NavBar';
 import Grid from './components/Grid';
 import Cart from './components/Cart';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { store } from './utils/store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        pauseOnHover={false}
-        closeOnClick
-        draggable
-      />
-      <NavBar />
+    <Provider store={store}>
+      <BrowserRouter>
+        <ToastContainer
+          position="top-right"
+          autoClose={2000}
+          pauseOnHover={false}
+          closeOnClick
+          draggable
+        />
+        <NavBar />
 
-      <Routes>
-        <Route path="/" element={<Grid />} />
-        <Route path="/sales" element={<Grid />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Grid />} />
+          <Route path="/sales" element={<Grid />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
