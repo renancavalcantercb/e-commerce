c876a9d80e5187dee7da79ef1f0a4fe4f409686a
unknown <renancavalcantercb@protonmail.com>
2023-06-05 03:58:32 -0300

    created and added ItemModal

--- frontend/src/components/Grid.js
+++ frontend/src/components/Grid.js
@@ -1,43 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateItems } from '../utils/store';
+import ItemModal from './ItemModal';
 
 const itemsPerPage = 8;
 const itemsPerPageMobile = 4;
-
-const localStorage = window.localStorage;
-
-const handleAddToCart = (item) => {
-    const cartItem = {
-        id: item._id.$oid,
-        title: item.title,
-        price: item.on_sale ? item.sale_price.toFixed(2) : item.price.toFixed(2),
-    };
-    console.log(cartItem)
-    const existingItems = JSON.parse(localStorage.getItem("cart")) || [];
-    const existingItemIndex = existingItems.findIndex((item) => item.id === cartItem.id);
-
-    if (existingItemIndex !== -1) {
-        existingItems.splice(existingItemIndex, 1);
-
-        const alertMessage = `O item "${cartItem.title}" já está no carrinho e foi removido.`;
-        toast.warn(alertMessage);
-    } else {
-        existingItems.push(cartItem);
-
-        const alertMessage = `O item "${cartItem.title}" foi adicionado ao carrinho.`;
-        toast.success(alertMessage);
-    }
-    localStorage.setItem("cart", JSON.stringify(existingItems));
-};
 
 function Grid() {
     const location = useLocation();
     const dispatch = useDispatch();
     const items = useSelector((state) => state);
     const [currentPage, setCurrentPage] = useState(1);
+    const [selectedItem, setSelectedItem] = useState(null);
 
     useEffect(() => {
         if (location.pathname === '/sales') {
@@ -74,12 +49,23 @@ function Grid() {
 
     const totalPages = Math.ceil(filteredItems.length / (window.innerWidth < 640 ? itemsPerPageMobile : itemsPerPage));
 
+    const openItemModal = (item) => {
+        setSelectedItem(item);
+    };
+
+    const closeItemModal = () => {
+        setSelectedItem(null);
+    };
+
     return (
         <div className="container mx-auto">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 py-4">
                 {currentItems.map((item) => (
                     <div key={item._id.$oid} className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col">
-                        <div className="flex-grow p-4 flex flex-col justify-between hover:bg-gray-100 cursor-pointer">
+                        <div
+                            onClick={() => openItemModal(item)}
+                            className="flex-grow p-4 flex flex-col justify-between hover:bg-gray-100 cursor-pointer"
+                        >
                             <img src={item.image} alt={item.title} className="w-full h-48 object-cover" />
                             <div>
                                 <h3 className="text-xl font-semibold mb-2">{item.title}</h3>
@@ -113,9 +99,9 @@ function Grid() {
                     </button>
                 ))}
             </div>
+
+            <ItemModal isOpen={selectedItem !== null} onClose={closeItemModal} item={selectedItem} />
         </div>
-
-
     );
 }
 
