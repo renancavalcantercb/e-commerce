98fbe55c2a0fd039ffc2229d42206b1e085dc502
unknown <renancavalcantercb@protonmail.com>
2023-09-03 15:32:27 -0300

    added login and register options

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,5 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Popover, Transition } from '@headlessui/react'
-import { Bars3Icon, ShoppingBagIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Popover } from '@headlessui/react'
+import { Bars3Icon, ShoppingBagIcon } from '@heroicons/react/24/outline'
 import SearchBar from './SearchBar.js'
 import { Link } from 'react-router-dom'
 
@@ -11,77 +10,15 @@ const navigation = {
         { name: 'Home', href: '/' },
         { name: 'Sales', href: '/sales' },
     ],
+    auth: [
+        { name: 'Sign in', href: '/login' },
+        { name: 'Create account', href: '/register' },
+    ]
 }
 
 export default function NavBar() {
-    const [open, setOpen] = useState(false)
-
     return (
         <div className="bg-white">
-            <Transition.Root show={open} as={Fragment}>
-                <Dialog as="div" className="relative z-40 lg:hidden" onClose={setOpen}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="transition-opacity ease-linear duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="transition-opacity ease-linear duration-300"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-25" />
-                    </Transition.Child>
-
-                    <div className="fixed inset-0 z-40 flex">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="transition ease-in-out duration-300 transform"
-                            enterFrom="-translate-x-full"
-                            enterTo="translate-x-0"
-                            leave="transition ease-in-out duration-300 transform"
-                            leaveFrom="translate-x-0"
-                            leaveTo="-translate-x-full"
-                        >
-                            <Dialog.Panel className="relative flex w-full max-w-xs flex-col overflow-y-auto bg-white pb-12 shadow-xl">
-                                <div className="flex px-4 pb-2 pt-5">
-                                    <button
-                                        type="button"
-                                        className="-m-2 inline-flex items-center justify-center rounded-md p-2 text-gray-400"
-                                        onClick={() => setOpen(false)}
-                                    >
-                                        <span className="sr-only">Close menu</span>
-                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                                    </button>
-                                </div>
-
-                                <div className="space-y-6 border-t border-gray-200 px-4 py-6">
-                                    {navigation.pages.map((page) => (
-                                        <div key={page.name} className="flow-root">
-                                            <a href={page.href} className="-m-2 block p-2 font-medium text-gray-900">
-                                                {page.name}
-                                            </a>
-                                        </div>
-                                    ))}
-                                </div>
-
-                                <div className="space-y-6 border-t border-gray-200 px-4 py-6">
-                                    <div className="flow-root">
-                                        <button className="-m-2 block p-2 font-medium text-gray-900">
-                                            Sign in
-                                        </button>
-                                    </div>
-                                    <div className="flow-root">
-                                        <button className="-m-2 block p-2 font-medium text-gray-900">
-                                            Create account
-                                        </button>
-                                    </div>
-                                </div>
-                            </Dialog.Panel>
-                        </Transition.Child>
-                    </div>
-                </Dialog>
-            </Transition.Root>
-
             <header className="relative bg-white">
                 <nav aria-label="Top" className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                     <div className="border-b border-gray-200">
@@ -89,46 +26,47 @@ export default function NavBar() {
                             <button
                                 type="button"
                                 className="rounded-md bg-white p-2 text-gray-400 lg:hidden"
-                                onClick={() => setOpen(true)}
                             >
                                 <span className="sr-only">Open menu</span>
                                 <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                             </button>
 
                             <div className="ml-4 flex lg:ml-0">
-                                <button onClick={() => window.location.href = "/"}>
+                                <Link to="/">
                                     <span className="sr-only">E-commerce</span>
                                     <img
                                         className="h-8 w-auto"
                                         src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                                         alt=""
                                     />
-                                </button>
+                                </Link>
                             </div>
 
                             <Popover.Group className="hidden lg:ml-8 lg:block lg:self-stretch">
                                 <div className="flex h-full space-x-8">
                                     {navigation.pages.map((page) => (
-                                        <a
+                                        <Link
                                             key={page.name}
-                                            href={page.href}
+                                            to={page.href}
                                             className="flex items-center text-sm font-medium text-gray-700 hover:text-gray-800"
                                         >
                                             {page.name}
-                                        </a>
+                                        </Link>
                                     ))}
                                 </div>
                             </Popover.Group>
 
                             <div className="ml-auto flex items-center">
                                 <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
-                                    <button className="text-sm font-medium text-gray-700 hover:text-gray-800">
-                                        Sign in
-                                    </button>
-                                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
-                                    <button className="text-sm font-medium text-gray-700 hover:text-gray-800">
-                                        Create account
-                                    </button>
+                                    {navigation.auth.map((authItem) => (
+                                        <Link
+                                            key={authItem.name}
+                                            to={authItem.href}
+                                            className="text-sm font-medium text-gray-700 hover:text-gray-800"
+                                        >
+                                            {authItem.name}
+                                        </Link>
+                                    ))}
                                 </div>
 
                                 {SearchBar()}
