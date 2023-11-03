6ef6503132796e389f0e423fe28f3fad88c388e3
unknown <renancavalcantercb@protonmail.com>
2023-11-03 04:37:57 -0300

    add flask-mail configuration

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -16,6 +16,12 @@ CORS(app)
 
 app.config["SECRET_KEY"] = environ.get("SECRET_KEY")
 app.config["MONGO_URI"] = environ.get("MONGO_URI")
+app.config["MAIL_SERVER"] = environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = environ.get("MAIL_PORT")
+app.config["MAIL_USERNAME"] = environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = environ.get("MAIL_PASSWORD")
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
 
 client = MongoClient(app.config["MONGO_URI"], connect=False)
 db = client["e-commerce"]
